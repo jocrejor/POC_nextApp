f9800a337365592e6c8ccf041137da9d7eb36c32
Manuel Pop <manpop2@alu.edu.gva.es>
2025-12-10 13:02:31 +0100

    afegit nom usuari actiu al dashboard també

--- bo/js/dashboard.js
+++ bo/js/dashboard.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", main);
 function main() {
     thereIsUser("login.html");
+     let sessioiniciada = localStorage.getItem("currentUser");
+
+    // Netejar la sessió si conté un objecte JSON mal format
+    if (sessioiniciada && (sessioiniciada.startsWith('{') || sessioiniciada.startsWith('['))) {
+        try {
+            sessdata = JSON.parse(sessioiniciada);
+            // Posar nom usuari al span
+            const spanUsuari = document.getElementById("nomUsuariActiu");
+            if (sessdata.name && spanUsuari) {
+                spanUsuari.textContent = sessdata.name;
+            }
+        } catch (e) {
+            console.error("Error parsejant dades de sessió", e);
+        }
+    }
 }
